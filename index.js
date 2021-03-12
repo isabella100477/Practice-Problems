86843622e2c34ae283beb8d47fdc66e1cfa0a040
Jonathan Corley <jcorley@noscihigh.org>
2021-03-12 20:28:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/* 
+1. Create a function factorial that when given a 
+number, returns the factorial of it. The factorial
+ of a number is the product of an integer and the 
+ integers below it.
+
+
+What are factorials? 
+https://en.wikipedia.org/wiki/Factorial
+
+For example:
+factorial(1); // => 1     (1)
+factorial(2); // => 2     (2 * 1)
+factorial(3); // => 6     (3 * 2 * 1)
+factorial(4); // => 24    (4 * 3 * 2 * 1)
+factorial(8); // => 40320 (8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
+
+
+
+  */
+
+function factorial(number){
+//CODE BELOW HERE
+
+
+
+//CODE ABOVE HERE
+}
+
+
+
+
+
+
+/*
+2. Create a function reverseString that takes in a string
+ and returns the reversed version.
+
+Example: 
+reverseString('Hello, World!'); // => '!dlroW ,olleH'
+
+*/
+
+function reverseString(string){
+  //CODE BELOW HERE
+
+
+
+  //CODE ABOVE HERE
+}
